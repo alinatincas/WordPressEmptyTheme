050e910d50629e9ccd4d1bdae51415ee3444dc90
Alina Tincas <alin0831@edu.eal.dk>
2019-02-11 13:14:00 +0100

    Added the menu function/widget to the WP

--- wp-content/themes/emptytheme/functions.php
+++ wp-content/themes/emptytheme/functions.php
@@ -1,7 +1,17 @@
 <?php
+    //Enabling the scripts
     function emptytheme_script_enqueue() {
         wp_enqueue_style('customstyle', get_template_directory_uri().'/css/emptytheme.css', array(), '1.0.0', 'all');
+        wp_enqueue_script('customjs', get_template_directory_uri().'/js/emptytheme.js', array(), '1.0.0', true);
     }
 
     add_action('wp_enqueue_scripts', 'emptytheme_script_enqueue');
+
+    //Activate some widgets on WP
+    function empty_theme_setup(){
+        add_theme_support('menus');
+        register_nav_menu( 'primary', 'Top Navigation' );
+        register_nav_menu( 'secondary', 'Bottom Navigation' );
+    }
+    add_action('init', 'empty_theme_setup');
 ?>
